01aa5536a6c15fa679a8a2539107a9f081384784
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2019-07-16 13:49:42 +0200

    fixup after cherrypick

--- src/main/sql/1-schema.sql
+++ src/main/sql/1-schema.sql
@@ -17,8 +17,16 @@ ALTER SEQUENCE product_order_id_seq RESTART WITH 101;
 CREATE TABLE order_line
 (
     id        SERIAL PRIMARY KEY,
-    order_id  SERIAL REFERENCES product_order(id),
-    reference SERIAL REFERENCES product(reference),
+    order_id  SERIAL REFERENCES product_order (id),
+    reference SERIAL REFERENCES product (reference),
     quantity  int NOT NULL default 1
 );
 ALTER SEQUENCE order_line_id_seq RESTART WITH 1001;
+
+CREATE TABLE product_margin
+(
+    reference INTEGER PRIMARY KEY,
+    total_margin    FLOAT
+);
+
+
---
+++ workshop2.md
@@ -0,0 +1,13 @@
+# Workshop 2: getBestSales() in a CQRS way
+
+_Goal:_ 
+Given the previous workshop1 e-commerce website, 
+build a backoffice service returning the "best sales" in a CQRS way
+
+## Create the product_margin table
+* create the table (schema.sql)
+* init the data (data.sql)
+
+## amend the order()  service
+
+## amend the getBestSales() service 
