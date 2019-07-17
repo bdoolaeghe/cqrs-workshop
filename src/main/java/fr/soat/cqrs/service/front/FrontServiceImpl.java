332d07c75e30867955bcff375b139368689625b1
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2019-07-17 19:00:00 +0200

    prepare code for workshop3

--- src/main/java/fr/soat/cqrs/service/front/FrontServiceImpl.java
+++ src/main/java/fr/soat/cqrs/service/front/FrontServiceImpl.java
@@ -3,10 +3,8 @@ package fr.soat.cqrs.service.front;
 import fr.soat.cqrs.dao.OrderDAO;
 import fr.soat.cqrs.dao.ProductDAO;
 import fr.soat.cqrs.dao.ProductMarginDAO;
-import fr.soat.cqrs.event.OrderSavedEvent;
 import fr.soat.cqrs.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +15,11 @@ public class FrontServiceImpl implements FrontService {
     private final ProductDAO productDAO;
     private final ProductMarginDAO productMarginDAO;
 
-    private final ApplicationEventPublisher eventPublisher;
-
     @Autowired
-    public FrontServiceImpl(OrderDAO orderDAO, ProductDAO productDAO, ProductMarginDAO productMarginDAO, ApplicationEventPublisher applicationEventPublisher) {
+    public FrontServiceImpl(OrderDAO orderDAO, ProductDAO productDAO, ProductMarginDAO productMarginDAO) {
         this.orderDAO = orderDAO;
         this.productDAO = productDAO;
         this.productMarginDAO = productMarginDAO;
-        this.eventPublisher = applicationEventPublisher;
     }
 
     @Override
@@ -32,8 +27,9 @@ public class FrontServiceImpl implements FrontService {
     public Long order(Order order) {
         // save order in product_order
         Long inserted = orderDAO.insert(order);
-        // notiffy subscribers
-        eventPublisher.publishEvent(new OrderSavedEvent(order));
+        // publish an OrderSavedEvent
+        //FIXME
+
         return inserted;
     }
 
