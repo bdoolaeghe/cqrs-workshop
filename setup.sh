a2718e1df0d0f5f50c6cdc3877fceac0a016dcc5
bdoolaeghe <bruno.doolaeghe@soat.fr>
2019-07-18 12:19:58 +0200

    fixup rollback in tsets fixup setup.sh upgrade spring
    # Conflicts:
    #	src/test/java/fr/soat/cqrs/service/backoffice/BackOfficeServiceImplTest.java

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #/bin/sh
-yes | sudo apt install openjdk-11-jdk
+yes | sudo apt install openjdk-11-jdk
 yes | sudo apt install docker
 yes | sudo apt install docker-compose
 yes | sudo apt install make
 
 ## add user to group docker
 sudo usermod -aG docker ${USER}
-su - ${USER}
+echo ""
+echo "please reboot to apply group changes (added to docker group)"
+echo "to check your groups: $> id -nG"
+echo "expected to belong to docker group"
 
 ## check user is in docker group. Else logout/login
-id -nG
+# su - ${USER}
+# id -nG
+# exit
+
+exit 0
