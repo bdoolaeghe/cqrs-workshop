539c67b0ac6ab830edd8eb6d717904bd8b4ea609
Bruno Doolaeghe <bruno.doolaeghe@soat.fr>
2019-07-22 12:12:08 +0200

    Update ProductMarginDAOImpl.java

--- src/main/java/fr/soat/cqrs/dao/ProductMarginDAOImpl.java
+++ src/main/java/fr/soat/cqrs/dao/ProductMarginDAOImpl.java
@@ -4,7 +4,7 @@ import fr.soat.cqrs.model.BestSales;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
+import org.springframework.dao.EmptyResultDataAccessException;
 import javax.sql.DataSource;
 
 @Component
@@ -19,13 +19,17 @@ public class ProductMarginDAOImpl implements ProductMarginDAO {
 
     @Override
     public BestSales getBestSales() {
-        return jdbcTemplate.queryForObject(
-                "SELECT product_name, " +
-                        " total_margin as product_margin " +
-                        "FROM product_margin " +
-                        "ORDER BY total_margin DESC " +
-                        "LIMIT 3",
-                new Object[0], new BestSalesMapper());
+        try {
+            return jdbcTemplate.queryForObject(
+                    "SELECT product_name, " +
+                            " total_margin as product_margin " +
+                            "FROM product_margin " +
+                            "ORDER BY total_margin DESC " +
+                            "LIMIT 3",
+                    new Object[0], new BestSalesMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return new BestSales();
+        }
     }
 
     @Override
