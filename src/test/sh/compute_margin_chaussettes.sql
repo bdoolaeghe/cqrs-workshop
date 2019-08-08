f90dbc7ca39d60c941e5089b236a7756228a4b84
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2019-08-08 20:30:21 +0200

    clean compute and compare sQL files (is in MD)

--- src/test/sh/compute_margin_chaussettes.sql
+++
@@ -1,4 +0,0 @@
--- total margin sur les chaussettes
-SELECT SUM((price - supply_price) * quantity) AS product_margin
-FROM product JOIN order_line ON product.reference = order_line.reference
-WHERE product.reference = 1;
--- src/test/sh/select_margin_chaussettes.sql
+++
@@ -1,4 +0,0 @@
--- total margin sur les cuassettes
-select *
-from product_margin
-where product_reference = 1;
